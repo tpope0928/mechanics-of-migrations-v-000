e7add2f7232e55cbe039dbabb8c528f44dc769a1
Thomas Pope <tpope0928@gmail.com>
2019-01-09 22:11:31 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,18 +1,11 @@
 class CreateArtists < ActiveRecord::Migration
-
-
-   def self.create_table
-    sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY,
-      title TEXT,
-      length INTEGER
-      )
-    SQL
-    ActiveRecord::Base.connection.execute(sql)
+  def up
   end
 
-   def change
+  def down
+  end
+
+  def change
     create_table :artists do |t|
       t.string :name
       t.string :genre
@@ -20,5 +13,4 @@ class CreateArtists < ActiveRecord::Migration
       t.string :hometown
     end
   end
-
- end 
+end
